f003f9edb8f0224afb40d4aa2023b032921faa26
davidboybob <davidboy7780@gmail.com>
2019-08-14 18:02:29 +0900

    190814|done

---
+++ 04_django/00_django_intro/pages/views.py
@@ -0,0 +1,75 @@
+###django imports style guide ###
+# 1. stndard library
+# 2. third-party
+# 3. django
+# 4. local django
+
+import random
+from datetime import datetime
+from django.shortcuts import render
+
+# Create your views here.
+def index(request): #첫번쨰 인자는 반드시 request
+    return render(request, 'index.html') # render()의 첫번째 인자도 반드시 request
+
+
+def introduce(request, name, age):
+    context = {'name': name, 'age': age}
+    return render(request, 'introduce.html', context)
+
+
+def dinner(request):
+    menu = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menu)
+    context = {'pick':pick}
+    return render(request, 'dinner.html', context)
+
+
+def image(request):
+    return render(request, 'image.html')
+
+
+def hello(request, name):
+    
+    menu = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menu)
+    context = {'name': name, 'pick': pick,}
+    return render(request, 'hello.html', context)
+
+
+def times(request, num, num2):
+    mul = num * num2
+    context = {'mul': mul, 'num': num, 'num2': num2, }
+    return render(request, 'times.html', context)
+
+def area(request, r):
+    area = 3.14 * r**2
+    context = {'area': area,}
+    return render(request, 'area.html', context)
+
+def template_language(request):
+    menus =['짜장면','탕수육', '짬뽕', '양장피',]
+    my_sentence = 'Life is short, you need python'
+    messages = ['apple', 'banana', 'cucumber', 'bean',]
+    datetimenow = datetime.now()
+    empty_list = []
+    context = {
+        'menus': menus,
+        'my_sentence': my_sentence,
+        'messages': messages,
+        'datetimenow': datetimenow,
+        'empty_list': empty_list,
+
+    }
+    return render(request, 'template_language.html', context)
+
+
+def isitgwangbok(request):
+    today = datetime.now()
+    if today.month == 8 and today.day == 15:
+        result = True
+    else:
+        result = False
+
+    context = {'result':result,}
+    return render(request, 'isitgwangbok.html', context)
--- SWexpert/D2/1859. 백만 장자 프로젝트.py
+++ SWexpert/D2/1859. 백만 장자 프로젝트.py
@@ -2,6 +2,19 @@ import sys
 sys.stdin = open('input_1859.txt', 'r')
 
 TC = int(input())
-for k in range(1, TC+1):
+for k in range(3):
     N = int(input())
+    lst = list(map(int, input().split()))
+    arr = [0] * N
+    buy_cnt = 0
+
+    for i in range(N):
+        for j in range(N):
+            if lst[i] <= lst[j]:
+                arr[i] += 1
+
+    print(arr)
+
+
+
 
