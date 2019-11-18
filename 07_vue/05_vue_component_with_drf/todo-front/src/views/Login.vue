28ced3b4cbde027da0e11d23703950c502885d59
davidboybob <davidboy7780@gmail.com>
2019-11-18 17:57:21 +0900

    vue_component_with_drf

---
+++ 07_vue/05_vue_component_with_drf/todo-front/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <LoginForm/>
+    
+  </div>
+</template>
+
+<script>
+  // @ 는 /src의 alias
+  // import LoginForm from '../components/LoginForm'
+  import LoginForm from '@/components/LoginForm'
+
+
+  export default {
+    name: 'login',
+    components: {
+      LoginForm,
+    }  
+  }
+</script>
+
+<style scoped>
+
+</style>
